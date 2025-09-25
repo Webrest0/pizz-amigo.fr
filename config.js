d637e49718e19b31c4ff4363a37a5254cd551063
Webrest0 <smarttlelearning@gmail.com>
2025-09-25 21:55:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
+/* === Configuration centrale PIZZ'AMIGO === */
+export const API_URL = "https://script.google.com/macros/s/AKfycbw_cAhR_LZgNRPzyABxZIWgqaYf-mBTPoisV8QgFgYUE-lVgI5pY-9_RFk_Z0w28itu/exec";
+
+// Si ton Apps Script demande une clé admin, mets-la ici. Sinon laisse vide.
+export const ADMIN_KEY = ""; 
 
