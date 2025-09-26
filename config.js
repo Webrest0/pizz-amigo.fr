0e6bd5743a20177c359af1a7431d3889ec42080f
Webrest0 <smarttlelearning@gmail.com>
2025-09-26 23:36:28 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-/* === Config centrale PIZZ'AMIGO === */
-export const API_URL = "https://script.google.com/macros/s/AKfycbw_cAhR_LZgNRPzyABxZIWgqaYf-mBTPoisV8QgFgYUE-lVgI5pY-9_RFk_Z0w28itu/exec";
-/* Si ton Apps Script nécessite une clé admin, mets-la ici, sinon laisse vide. */
-export const ADMIN_KEY = ""; // ex: "PIZZAMIGO-ADMIN-123"
